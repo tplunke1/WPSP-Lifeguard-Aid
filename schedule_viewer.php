24f4a59bbca4942d711261446b69ce7c4f75938a
tplunke1 <tplunke1@vols.utk.edu>
2016-06-26 01:54:03 -0400

    Switched to PDO

--- schedule_viewer.php
+++ schedule_viewer.php
@@ -1,30 +1,24 @@
 <?php
-$servername = "Pockets-PC";
+$dsn = "mysql:host=Pockets-PC;dbname=wpsp_lifeguard_db;charset=utf8mb4";
 $username = "tplunke1";
 $password = "t20o16sms";
 $dbname = "wpsp_lifeguard_db";
 
 // Connect to database
-$conn = new mysqli($servername, $username, $password, $dbname);
-if($conn->connect_error)
-{
-	die("Connection failed: " . $conn->connect_error);
-}
-else
-{
-	echo "Connected. <br>";
-}
+$db = new \PDO($dsn, $username, $password);
 
 /*
 $date_input = "1-7-2016";
 $date = date('Y-m-d', strtotime($date_input));
 */
 
-$sql = "SELECT * FROM lifeguard_leave WHERE CURDATE() < start_date OR CURDATE() > end_date";
-$result = $conn->query($sql);
+$sql = "SELECT lifeguard_id FROM lifeguard_leave WHERE CURDATE() < start_date OR CURDATE() > end_date";
+$stmt = $db->query($sql);
+$row_count = $stmt->rowCount();
+echo 'lifeguards available: ' . $row_count . '<br>';
 
-while($row = $result->fetch_assoc())
+while($row = $stmt->fetch(PDO::FETCH_ASSOC))
 {
-	echo $row["lifeguard_id"];
+	echo $row['lifeguard_id'];
 }
 ?>
